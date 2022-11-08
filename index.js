dcd9d296857dab09c302c84a3088ed9dba24a669
DevRefaul <devrefaul@gmail.com>
2022-11-09 02:26:46 +0600

    created mongodb database and connected the database and secure with env file

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express()
-
+require('dotenv').config()
 // middle wares
 app.use(cors())
 app.use(express.json())
 
 
+// mongo configure
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w9zclsg.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri);
 
+const run = async () => {
+    try {
+
+        const tourDestinations = client.db('tourPlaces').collection('destinations')
+
+        app.get('/tours', async (req, res) => {
+            const query = {}
+            const result = await tourDestinations.find(query).toArray()
+            res.send({
+                status: "success",
+                data: result
+            })
+        })
+
+    } catch (error) {
+
+    }
+}
+run()
 // apis
 app.get('/', (req, res) => {
     res.send('Server is Up and Running')
